ca872b2a2586319dfc8827a09288c45b8bc30b16
Gouravsharm1 <66167338+Gouravsharm1@users.noreply.github.com>
2021-07-09 18:16:19 +0530

    Update features.js

--- public/features.js
+++ public/features.js
@@ -1,6 +1,4 @@
-
-
-// function to mute or unmute self audio parameter a with be changed 
+// function to mute or unmute. Self audio parameter a will be changed 
 const muteunmute= ()=> {
     const ena= myvidstr.getAudioTracks()[0].enabled ;  // checking the current condition
     if(ena){
@@ -15,48 +13,20 @@ const muteunmute= ()=> {
     }
 }
 
-// to change the appearance of button after unmute operation
-const setmutebut= () =>
-{
-    var v= document.getElementById("mute_button") ;
-    v.style.color = "green" ;
-}
-
-// to change the appearance of button after mute operation
-const setunmutebut= () =>
-{
-    var v= document.getElementById("mute_button") ;
-    v.style.color = "red" ;
-}
-
 //function to stop or to show self video . video parameter v will be changed. 
 const camoff= ()=> {
     const ena= myvidstr.getVideoTracks()[0].enabled ;
     if(ena){
         myvidstr.getVideoTracks()[0].enabled= false ;
         v=false ;
-        setoffbut() ;
+        setonbut() ;
     }
     else{
         myvidstr.getVideoTracks()[0].enabled= true ;
         v=true ;
-        setonbut() ;
+        setoffbut() ;
     }
 }
-
-// change appearance of button
-const setoffbut= () =>
-{
-    var v= document.getElementById("camera_button") ;
-    v.style.color = "red" ;
-}
-
-const setonbut= () =>
-{
-    var v= document.getElementById("camera_button") ;
-    v.style.color = "green" ;
-}
-
 
 // function to copy the url address of meeting
 function Copy(text){
@@ -70,21 +40,22 @@ inputc.parentNode.removeChild(inputc);
 
 }
 
-// function to leave the meeting. this will redirect to home page of website
+// function to leave the meeting. This will redirect to home page of website
 const leave= ()=>{
 
     var confirm_result = confirm("Are you sure you want to leave? ");
         if (confirm_result == true) {
-            socket.disconnect() ;
+            socket.disconnect(name) ;
             location.replace("https://vichill.herokuapp.com")
         }
 }
 
-
 // function to send inviting email. email will be sent by user. subject and content will be set by us.
 function sendEmail() {
+
     var subject = "Inviting to chill out at ViChill";
-    var emailBody = 'Hi %0D%0A' + 'I '+name+' am inviting you to come to our meeting by clicking '+ window.location.href 
+    var emailBody = 'Hi %0D%0A' + 'I '+name+' invite you to come to our meeting by clicking '+ window.location.href 
     + ' %0D%0ANote : you can always start a new meeting by going to https://vichill.herokuapp.com' ;
-    document.location = "mailto:?subject="+subject+"&body="+emailBody;
+    window.open("mailto:?subject="+subject+"&body="+emailBody) ;
+    
 }
